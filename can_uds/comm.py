7d73cf19d3b2e7869effff697ff70baa88ce25fc
Sho Nakatani <lay.sakura@gmail.com>
2024-08-10 05:58:54 +0900

    fix: more conformant

--- can_uds/comm.py
+++ can_uds/comm.py
@@ -16,6 +16,18 @@ def send_recv(sock: isotp.socket, req: bytes) -> bytes:
 
 def is_positive_resp(resp: bytes, req_sid: int) -> bool:
     """
+    Positive response has at least 1 byte. This function checks it.
+
+    - SID (request SID + 0x40)
+    """
+    if len(resp) == 0:
+        return False
+
+    return resp[0] == req_sid + 0x40
+
+
+def is_negative_resp(resp: bytes, req_sid: int) -> bool:
+    """
     Negative response has at least 3 bytes. This function checks them.
 
     - SID (0x7F)
@@ -23,9 +35,9 @@ def is_positive_resp(resp: bytes, req_sid: int) -> bool:
     - NRC
     """
     if len(resp) < 3:
-        return True
+        return False
 
     if resp[0] != 0x7F:
-        return True
+        return False
 
-    return resp[1] != req_sid
+    return resp[1] == req_sid
