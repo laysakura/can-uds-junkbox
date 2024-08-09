8106c2177b5f36026007e62b0f2520c4b5d5c71b
Sho Nakatani <lay.sakura@gmail.com>
2024-08-09 13:10:16 +0900

    chore: sleep after reset

--- examples/automotive-ctf-2023/User-Space-Diagnostics/Read-Data-By-Identifier.py
+++ examples/automotive-ctf-2023/User-Space-Diagnostics/Read-Data-By-Identifier.py
@@ -3,11 +3,12 @@ from can_uds.uds import (
     read_memory_by_id,
     reset_ecu,
 )
-
+import time
 
 if __name__ == "__main__":
     sock = create_socket("vcan0", 0x7E0, 0x7E8, timeout=2.0)
     reset_ecu(sock)
+    time.sleep(5.0)
 
     for id_ in range(0, 0xFFFF):
         resp = read_memory_by_id(sock, id_)
