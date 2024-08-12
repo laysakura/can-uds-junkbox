7e2e48734bf9cecbb9997374cff842e0a540b658
Sho Nakatani <lay.sakura@gmail.com>
2024-08-12 10:07:01 +0900

    refactor

--- examples/automotive-ctf-2023/User-Space-Diagnostics/Security-Access-Level-5.py
+++ examples/automotive-ctf-2023/User-Space-Diagnostics/Security-Access-Level-5.py
@@ -11,30 +11,6 @@ from can_uds.uds import (
 )
 
 
-def calc_key_level1(seed: bytes, xor_by: int) -> bytes:
-    key = int.from_bytes(seed, "big") & 0xFFFFFFFF
-
-    # 4バイト、全て xor_by で埋める
-    xor = xor_by << 24 | xor_by << 16 | xor_by << 8 | xor_by
-
-    key ^= xor
-    return key.to_bytes(4, "big")
-
-
-def solve_level1(sock: isotp.socket, xor_by: int):
-    sec_access = SecurityAccess(sock, 0x01)
-    seed = sec_access.request_seed()
-    assert len(seed) == 4
-    print(f"[level1] Seed: {seed.hex()}")
-
-    key = calc_key_level1(seed, xor_by)
-    print(f"[level1] Key: {key.hex()} (xor_by = {xor_by})")
-    ret = sec_access.send_key(key)
-    if ret:
-        print(f"[level1] Security Access Level1 succeeded (xor_by = {xor_by})")
-        return True
-
-
 def solve_level5(sock: isotp.socket):
     """
     メルセンヌ・ツイスタをcrackする。
